142b60906d0a58cb23ea7f39bb5635c0f4942f4d
[username] <[547114121@qq.com}>
2017-06-06 22:46:45 +0800

    6.6.

--- js/js1.js
+++ js/js1.js
@@ -1,31 +1,22 @@
 /**
  * Created by Administrator on 2017/6/1.
  */
-var EventUtil = {
-    addHandler :function (element, type ,handler) {
-        if(element.addEventListener){
-            element.addEventListener(type,handler,false);
-        } else if (element.attachEvent){
-            element.attachEvent("on" + type,handler);
-        } else{
-            element["on" + type] =handler;
-        }
-    },
-    removeHandler:function (element ,type,handler) {
-        if(element.removeEventListener){
-            element.removeEventListener(type,handler,false);
-        } else if (element.detachEvent){
-            element.detachEvent("on" + type,handler);
-        } else{
-            element["on" + type] =null;
-        }
-    }
-};
+window.onload=function () {
+    //获取九宫格
     var square = document.getElementsByClassName("square");
+    //获取开始闪按钮
     var btnOn = document.getElementById("btn1");
+    //获取结束闪按钮
     var btnOff = document.getElementById("btn2");
+    //间歇时间
     var interval =null;
+
+    //设置随机颜色
+    var getRandomColor = function(){
+        return '#'+('00000'+(Math.random()*0x1000000<<0).toString(16)).substr(-6);
+    };
     function changeBgcolor() {
+        //获取九宫格中的3个格子，而且3个格子不重复
         var arr = [];
         while(arr.length < 3){
             var bFlag = true;
@@ -33,7 +24,7 @@ var EventUtil = {
             if(arr.length == 0){
                 arr.push(number);
             }
-            for(var i=0;i<arr.length;i++){
+            for(var i=0; i<arr.length; i++){
                 if(number == arr[i]){
                     bFlag = false;
                 }
@@ -42,27 +33,35 @@ var EventUtil = {
                 arr.push(number);
             }
         }
-        for(var i =0; i<9; i++) {
-            square[i].style.backgroundColor = '#EEB422';
+        //初始化所有格子的颜色
+        for(var i =0; i< square.length ; i++) {
+            square[i].style.backgroundColor='#EEB422';
         }
+        //取得随机颜色，并将颜色赋值给选好的3个格子
         for(var i=0; i<3 ; i++){
             var a =arr[i];
-            var getRandomColor = function(){
-                return '#'+('00000'+(Math.random()*0x1000000<<0).toString(16)).substr(-6);
-            };
             square[a].style.backgroundColor=getRandomColor();
         }
+        //开始闪按钮不能点击
+        btnOn.disabled=true;
+        btnOn.style.cursor="wait";
     }
+    //设置循环函数
     function circulate() {
         interval = setInterval(changeBgcolor,1000);
     }
     function resetBgcolor() {
+        //清除循环
         clearInterval(interval);
         for(var i=0; i<9 ; i++){
             square[i].style.backgroundColor='#EEB422';
         }
+        //开始闪按钮能点击
+        btnOn.disabled=false;
+        btnOn.style.cursor="pointer";
     }
-    EventUtil.addHandler(btnOn,"load",changeBgcolor());
-    EventUtil.addHandler(btnOff,"load",resetBgcolor());
+    btnOn.onclick=circulate;
+    btnOff.onclick=resetBgcolor;
+};
 
 
