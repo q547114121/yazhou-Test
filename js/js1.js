63934389363fbe573adcae836d77002704db2546
[username] <[547114121@qq.com}>
2017-06-08 22:44:36 +0800

    6.7

--- js/js1.js
+++ js/js1.js
@@ -15,43 +15,51 @@ window.onload=function () {
     var getRandomColor = function(){
         return '#'+('00000'+(Math.random()*0x1000000<<0).toString(16)).substr(-6);
     };
-    function changeBgcolor() {
-        //获取九宫格中的3个格子，而且3个格子不重复
-        var arr = [];
-        while(arr.length < 3){
-            var bFlag = true;
-            var number = Math.floor(Math.random()*9+0);
-            if(arr.length == 0){
-                arr.push(number);
-            }
-            for(var i=0; i<arr.length; i++){
-                if(number == arr[i]){
-                    bFlag = false;
+
+
+        function changeBgcolor() {
+
+            // clearInterval(interval);
+            //获取九宫格中的3个格子，而且3个格子不重复
+            var arr = [];
+            while(arr.length < 3){
+                var bFlag = true;
+                var number = Math.floor(Math.random()*9+0);
+                if(arr.length == 0){
+                    arr.push(number);
+                }
+                for(var i=0; i<arr.length; i++){
+                    if(number == arr[i]){
+                        bFlag = false;
+                    }
+                }
+                if(bFlag){
+                    arr.push(number);
                 }
             }
-            if(bFlag){
-                arr.push(number);
+            //初始化所有格子的颜色
+            for(var i =0; i< square.length ; i++) {
+                square[i].style.backgroundColor='#EEB422';
             }
+            //取得随机颜色，并将颜色赋值给选好的3个格子
+            for(var i=0; i<3 ; i++){
+                var a =arr[i];
+                square[a].style.backgroundColor=getRandomColor();
+            }
+            //开始闪按钮不能点击
+            btnOn.disabled=true;
+            btnOn.style.cursor="wait";
         }
-        //初始化所有格子的颜色
-        for(var i =0; i< square.length ; i++) {
-            square[i].style.backgroundColor='#EEB422';
-        }
-        //取得随机颜色，并将颜色赋值给选好的3个格子
-        for(var i=0; i<3 ; i++){
-            var a =arr[i];
-            square[a].style.backgroundColor=getRandomColor();
-        }
-        //开始闪按钮不能点击
-        btnOn.disabled=true;
-        btnOn.style.cursor="wait";
-    }
+
     //设置循环函数
     function circulate() {
+        clearInterval(interval);
         interval = setInterval(changeBgcolor,1000);
     }
+
+    //清除循环
     function resetBgcolor() {
-        //清除循环
+
         clearInterval(interval);
         for(var i=0; i<9 ; i++){
             square[i].style.backgroundColor='#EEB422';
---
+++ js/js2-1.js
@@ -0,0 +1,94 @@
+/**
+ * Created by Administrator on 2017/6/7.
+ */
+window.onload=function () {
+    //取得input的value值
+    var input =document.getElementById("input");
+    var num = parseInt(input.value);
+    var person =document.getElementById("person-num");
+    var wolf =document.getElementById("wolf-num");
+    input.addEventListener("change",setNum,false);
+    function setNum(event) {
+        //取得该input元素
+        var target = event.target;
+        //设置正则表达式 测试是否玩家个数处于4-18
+        var pattern =/^([4-9]|(1[0-8]))$/;
+        if(!pattern.test(target.value)){
+            window.confirm("请输入正确的玩家数量。");
+            target.value="";
+        }
+        //根据玩家数目，取得狼人和水民的数目
+        var value =parseInt(target.value);
+        switch (true){
+                    case value >= 6 && value<=8:
+                        wolf.innerHTML =1;
+                        person.innerHTML= tartarget - 1;
+                        break;
+                    case value>=9 &&value<=11:
+                        wolf.innerHTML = 2 ;
+                        person.innerHTML = value-2;
+                        break;
+                    case value>=12 && value<=15:
+                        wolf.innerHTML = 3;
+                        person.innerHTML =value -3;
+                        break;
+                    case value>=16 && value<=18:
+                        wolf.innerHTML =4;
+                        person.innerHTML= value-4;
+                }
+        //设置一个数组存玩家数量，分配玩家数量
+        var arr = [] ;
+        //获得狼人的数量
+        var wolfNum = Number(wolf.innerHTML);
+        //获得水民的数量
+        var personNum =Number(person.innerHTML);
+        //把所有狼人传入数组
+        for(var i=0 ;i<wolfNum;i++){
+            arr.push("狼人");
+        }
+        //把所有水民传入数组
+        for (var j=0;j<personNum;j++){
+            arr.push("水民");
+        }
+        //设置洗牌函数，把数组打乱
+        function shuffle(array) {
+            var length=array.length;
+            var shuffled= Array(length);
+            for(var index=0,rand;index <length ;index++){
+                rand=parseInt(Math.random()*(index+1));
+                shuffled[index]=shuffled[rand];
+                shuffled[rand]=array[index]
+            }
+            return shuffled;
+        }
+        //调用洗牌函数
+
+        alert(shuffle(arr));
+    }
+    // input.onkeyup=function () {
+    //     switch (true){
+    //         case parseInt(input.value) >= 6 && parseInt(input.value)<=8:
+    //             wolf.innerHTML =1;
+    //             person.innerHTML= num - 1;
+    //             break;
+    //         case num>=9 &&num<=11:
+    //             wolf.innerHTML = 2 ;
+    //             person.innerHTML = num-2;
+    //             break;
+    //         case num>=12 && num<=15:
+    //             wolf.innerHTML = 3;
+    //             person.innerHTML =num -3;
+    //             break;
+    //         case num>=16 && num<=18:
+    //             wolf.innerHTML =4;
+    //             person.innerHTML= num-4;
+    //     }
+    // };
+
+
+
+
+
+
+
+};
